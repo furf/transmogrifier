6faf1ef38a53333478f6952e66e83eb78dc73e0a
David Furfero <furf@furfBook-Pro.local>
2009-10-27 13:35:01 -0400

    tighter code, fixed deeply nested bug

--- transmogrifier.js
+++ transmogrifier.js
@@ -5,39 +5,41 @@
 function Transmogrifier (map) {
 
   var EMPTY  = '',
-      SOURCE = 's',
-      TARGET = 't',
+      INPUT  = 'i',
+      OUTPUT = 'o',
       DOT    = '.',
       EQUALS = '=',
       OBJECT = '{}',
       BREAK  = ';\n',
-      NUMERIC_INDEX_REGEXP = /\.(\d+)/g,
+      numericIndexRegExp = /\.(\d+)/g,
       modifiers = [];
 
-  function makeSource (map /*, root */) {
+  function makeSource (map /*, ns */) {
 
-    var root    = arguments[1] || EMPTY,
-        rootDot = (root !== EMPTY) ? root + DOT : EMPTY,
-        src     = [],
+    var ns  = arguments[1] || OUTPUT,
+        src = [],
         prop,
         val,
         i;
 
     // Open function
-    if (root === EMPTY) {
-      src.push('var ', TARGET, EQUALS, OBJECT, BREAK);
-
-    // Render nested object
-    } else {
-      src.push(TARGET, DOT, root, EQUALS, OBJECT, BREAK);
+    if (ns === OUTPUT) {
+      src.push('var ');
     }
+
+    src.push(ns, EQUALS, OBJECT, BREAK);
 
     // Iterate map properties
     for (prop in map) {
       if (map.hasOwnProperty(prop)) {
+
+        // Extract value from mapped property
         val = map[prop];
 
-        // Traverse nested objects
+        // Render property as complete path for recursion and assignment
+        prop = ns + DOT + prop;
+
+        // Recurse nested objects
         if (typeof val === 'object') {
           src.push(makeSource(val, prop));
 
@@ -45,12 +47,14 @@ function Transmogrifier (map) {
         } else {
 
           // Render left-hand assignment
-          src.push(TARGET, DOT, rootDot, prop, EQUALS);
+          src.push(prop, EQUALS);
 
           // Render modifier
           if (typeof val === 'function') { 
+
+            // Cache modifier, referenceable by property 
             modifiers[prop] = val;
-            src.push('this.modifiers.', prop, '(', SOURCE, ')', BREAK);
+            src.push('this.modifiers["', prop, '"](', INPUT, ')', BREAK);
 
           // Render dot-delimited property as gated assignment
           } else {
@@ -58,24 +62,25 @@ function Transmogrifier (map) {
             val = val.split(DOT);
 
             for (i = val.length - 1; i >= 0; --i) {
-              val[i] = SOURCE + DOT + val.slice(0, i + 1).join(DOT);
+              val[i] = INPUT + DOT + val.slice(0, i + 1).join(DOT);
             }
 
-            src.push(val.join('&&').replace(NUMERIC_INDEX_REGEXP, '[$1]'), BREAK);
+            src.push(val.join('&&').replace(numericIndexRegExp, '[$1]'), BREAK);
           }
         }
       }
     }
 
     // Close function
-    if (root === EMPTY) {
-      src.push('return ', TARGET, BREAK);
+    if (ns === OUTPUT) {
+      src.push('return ', OUTPUT, BREAK);
     }
 
     return src.join(EMPTY);
   }
 
-  this.one = new Function(SOURCE, makeSource(map));
+  // Create new function from source and attach modifiers array
+  this.one = new Function(INPUT, makeSource(map));
   this.modifiers = modifiers;
 }
 
