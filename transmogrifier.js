2a566bc3db4b06786a107899e5a5dc0bdd03b509
David Furfero <furf@furfBook-Pro.local>
2009-10-28 13:30:53 -0400

    update

--- transmogrifier.js
+++ transmogrifier.js
@@ -1,33 +1,69 @@
+
+
+              /*=          :=?ZNNN=   ?NZ      ?$I?=~:        
+              INNNNNNNNNNNNNNNNNN    ZNN=     ZNNNNNNNNNZ=   
+              =NNZZZZZZZZZZZZZNZ    ?NNNN:    ZNZZNNNZZZNNN? 
+              ~NNNNNNNNNNZZZNNI     NNZZNZ    INZZN+:ZNNZNNN~
+                     ZNNZZZNN+     ?NNZZNNI   INZZN?  ZNZZNN=
+                     NNZZZZNZ      $NNZZZNN:  ?NNZNI  NNZZZN?
+                     NZZZZN+      =NNZZZZNNZ  $NNZNNZNNZZZNZ 
+                    NZZZZNN     ~:NNZZZNZZNN  INNZZZZZZZNNZ  
+                   $NZZNNNZNNNNNZZNNZZZZZZZN: INNZNNNNNN+    
+                  $NNZNNHTTP://BIT.LY/Z1n2dZN ?NNZNZ         
+                  NNZZZZZZZZZZNN~NNZNNNNZZZZN?=NNZN$         
+                ~NNZZZZZZZZZZZNNZNZNNI ZNZZZNNINNZNZ
+               ~NNNNNNNNNNNZZZZNNZZNNI INNZZZNNNNZNN=
+               ZNZI+~          NNZZNNI ~NNZZNNNNNZNN$
+                              NNNZZNNI ~NNNNNNINNZNNN:
+                             ZNNNNNNN?         ZNNNNZ 
+                             :II+:             $NZI*/
+
+
 /**
  * Transmogrifier
- * @author Dave Furfero
+ * Transmogrifier instances can be used to transmogrify, or transform, data
+ * from one object structure to another. Useful for simplifying or sanitizing
+ * large data feeds.
+ * 
+ * trans·mog·ri·fy
+ * trăns-mŏg'rə-fī', trănz-
+ * tr.v, -fied, -fy·ing, -fies.
+ * To change into a different shape or form, especially one that is fantastic
+ * or bizarre.
+ *
+ * @class
+ * @param {Object} map
+ * @constructs
+ * @author Dave Furfero <a href="mailto:furf@furf.com">furf@furf.com</a>
  */
 function Transmogrifier (map) {
 
-  var EMPTY  = '',
-      INPUT  = 'i',
-      OUTPUT = 'o',
-      DOT    = '.',
-      EQUALS = '=',
-      OBJECT = '{}',
-      BREAK  = ';\n',
-      numericIndexRegExp = /\.(\d+)/g,
-      modifiers = [];
+  var INPUT               = 'i',
+      OUTPUT              = 'o',
+      EMPTY               = '',
+      DOT                 = '.',
+      EQUALS              = '=',
+      OBJECT              = '{}',
+      ARRAY               = '[]',
+      BREAK               = ';',
+      ARRAY_INDEX_SEARCH  = /\.(\d+)/g,
+      ARRAY_INDEX_REPLACE = '[$1]',
+      dials               = [],
+      toString            = Object.prototype.toString;
 
   function makeSource (map /*, ns */) {
 
-    var ns  = arguments[1] || OUTPUT,
-        src = [],
-        prop,
-        val,
-        i;
+    var ns   = arguments[1] || OUTPUT,
+        type = (toString.call(map) === '[object Array]') ? ARRAY : OBJECT,
+        src  = [],
+        prop, val, i;
 
     // Open function
     if (ns === OUTPUT) {
       src.push('var ');
     }
 
-    src.push(ns, EQUALS, OBJECT, BREAK);
+    src.push(ns, EQUALS, type, BREAK);
 
     // Iterate map properties
     for (prop in map) {
@@ -37,10 +73,13 @@ function Transmogrifier (map) {
         val = map[prop];
 
         // Render property as complete path for recursion and assignment
-        prop = ns + DOT + prop;
+        // (w/ array index correction)
+        prop = (ns + DOT + prop).replace(ARRAY_INDEX_SEARCH, ARRAY_INDEX_REPLACE);
 
         // Recurse nested objects
         if (typeof val === 'object') {
+
+          // Special treatment for arrays
           src.push(makeSource(val, prop));
 
         // Render dot-delimited properties and modifiers
@@ -50,22 +89,23 @@ function Transmogrifier (map) {
           src.push(prop, EQUALS);
 
           // Render modifier
-          if (typeof val === 'function') { 
+          if (toString.call(val) === '[object Function]') {
 
-            // Cache modifier, referenceable by property 
-            modifiers[prop] = val;
-            src.push('this.modifiers["', prop, '"](', INPUT, ')', BREAK);
+            // Cache modifier, referenceable by property
+            dials[prop] = val;
+            src.push('this.dials["', prop, '"](', INPUT, ')', BREAK);
 
           // Render dot-delimited property as gated assignment
           } else {
 
-            val = val.split(DOT);
+            val = (EMPTY + val).split(DOT);
 
             for (i = val.length - 1; i >= 0; --i) {
               val[i] = INPUT + DOT + val.slice(0, i + 1).join(DOT);
             }
 
-            src.push(val.join('&&').replace(numericIndexRegExp, '[$1]'), BREAK);
+            // Join partial checks (w/ array index correction)
+            src.push(val.join('&&').replace(ARRAY_INDEX_SEARCH, ARRAY_INDEX_REPLACE), BREAK);
           }
         }
       }
@@ -79,18 +119,22 @@ function Transmogrifier (map) {
     return src.join(EMPTY);
   }
 
-  // Create new function from source and attach modifiers array
-  this.one = new Function(INPUT, makeSource(map));
-  this.modifiers = modifiers;
+  // Create new function from source and attach dials array
+  this.zap   = new Function(INPUT, makeSource(map));
+  this.dials = dials;
 }
 
-Transmogrifier.prototype.many = function(data) {
-  var i, n, arr = [];
-  if (!(data instanceof Array)) {
-    data = [data];
+Transmogrifier.prototype = {
+
+  nuke: function Transmogrifier_nuke (data) {
+    var i, n, arr = [];
+    if (Object.prototype.toString.call(data) !== '[object Array]') {
+      data = [data];
+    }
+    for (i = 0, n = data.length; i < n; ++i) {
+      arr.push(this.zap(data[i]));
+    }
+    return arr;
   }
-  for (i = 0, n = data.length; i < n; ++i) {
-    arr.push(this.one(data[i]));
-  }
-  return arr;
+
 };
