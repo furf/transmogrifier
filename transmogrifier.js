f196ab6b6d2ac12718341650630454924aa26b4d
David Furfero <furf@furfBook-Pro.local>
2009-10-29 15:04:06 -0400

    replaced push with string concat for size and speed improvements

--- transmogrifier.js
+++ transmogrifier.js
@@ -38,34 +38,26 @@
  */
 function Transmogrifier (map) {
 
-  var INPUT               = 'i',
-      OUTPUT              = 'o',
-      EMPTY               = '',
-      DOT                 = '.',
-      EQUALS              = '=',
-      OBJECT              = '{}',
-      ARRAY               = '[]',
-      BREAK               = ';',
-      ARRAY_INDEX_SEARCH  = /\.(\d+)/g,
-      ARRAY_INDEX_REPLACE = '[$1]',
+  var arrayIndexRegExp    = /^\d+$/,
+      chunker             = /(\.?[a-zA-Z_$][\w$]*)|(?:(?:\[(["']?))(?:([a-zA-Z_$][\w$]*)|(\d+)|([^"']+))(?:\2\]))/g,
       dials               = [],
       toString            = Object.prototype.toString;
 
   function makeSource (map /*, ns */) {
 
-    var ns   = arguments[1] || OUTPUT,
-        type = (toString.call(map) === '[object Array]') ? ARRAY : OBJECT,
-        src  = [],
-        prop, val, i;
+    var ns   = arguments[1] || 'o',
+        type = (toString.call(map) === '[object Array]') ? '[]' : '{}',
+        src,
+        prop,
+        val,
+        props,
+        parts,
+        p;
 
-    // Open function
-    if (ns === OUTPUT) {
-      src.push('var ');
-    }
+    // Render namespace
+    src = ns + '=' + type + ';';
 
-    src.push(ns, EQUALS, type, BREAK);
-
-    // Iterate map properties
+    // Iterate mapped properties
     for (prop in map) {
       if (map.hasOwnProperty(prop)) {
 
@@ -74,67 +66,73 @@ function Transmogrifier (map) {
 
         // Render property as complete path for recursion and assignment
         // (w/ array index correction)
-        prop = (ns + DOT + prop).replace(ARRAY_INDEX_SEARCH, ARRAY_INDEX_REPLACE);
+        prop = ns + (arrayIndexRegExp.test(prop) ? '[' + prop + ']' : '.' + prop);
 
         // Recurse nested objects
         if (typeof val === 'object') {
-
-          // Special treatment for arrays
-          src.push(makeSource(val, prop));
+          
+          src += makeSource(val, prop);
 
         // Render dot-delimited properties and modifiers
         } else {
 
           // Render left-hand assignment
-          src.push(prop, EQUALS);
+          src += prop + '=';
 
           // Render modifier
           if (toString.call(val) === '[object Function]') {
 
             // Cache modifier, referenceable by property
             dials[prop] = val;
-            src.push('this.dials["', prop, '"](', INPUT, ')', BREAK);
+            
+            // Render right-hand assignment by function
+            src += 'this.dials["' + prop + '"](i)' + ';';
 
           // Render dot-delimited property as gated assignment
           } else {
 
-            val = (EMPTY + val).split(DOT);
+            props = parts = '';
+            
+            /**
+             * 0 = match
+             * 1 = property (dot-notation)
+             * 2 = quote
+             * 3 = property (bracket-notation, clean)
+             * 4 = index (bracket-notation)
+             * 5 = property (bracket-notation, dirty)
+             */
+            while ((p = chunker.exec(val)) !== null) {
 
-            for (i = val.length - 1; i >= 0; --i) {
-              val[i] = INPUT + DOT + val.slice(0, i + 1).join(DOT);
+              // Optimize properties (remove brackets/quotes where unnecessary)
+              props += p[1] || p[3] && '.' + p[3] || '[' + (p[4] || '"' + p[5] + '"') + ']';
+
+              // Join gated assignment
+              parts += '&&i.' + props;
             }
 
-            // Join partial checks (w/ array index correction)
-            src.push(val.join('&&').replace(ARRAY_INDEX_SEARCH, ARRAY_INDEX_REPLACE), BREAK);
+            // Render right-hand assignment by property
+            src += parts.substr(2) + ';';
           }
         }
       }
     }
 
-    // Close function
-    if (ns === OUTPUT) {
-      src.push('return ', OUTPUT, BREAK);
-    }
-
-    return src.join(EMPTY);
+    // Wrap function
+    return (ns === 'o') ? 'var ' + src + 'return o;' : src;
   }
 
   // Create new function from source and attach dials array
-  this.zap   = new Function(INPUT, makeSource(map));
+  this.zap   = new Function('i', makeSource(map));
   this.dials = dials;
 }
 
 Transmogrifier.prototype = {
 
-  nuke: function Transmogrifier_nuke (data) {
+  nuke: function (data) {
     var i, n, arr = [];
-    if (Object.prototype.toString.call(data) !== '[object Array]') {
-      data = [data];
-    }
     for (i = 0, n = data.length; i < n; ++i) {
       arr.push(this.zap(data[i]));
     }
     return arr;
   }
-
 };
